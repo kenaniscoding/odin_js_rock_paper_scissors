349abd34c08c96f2632908ac0024ce64634332a1
kenaniscoding <banalkenan@gmail.com>
2025-07-21 08:55:04 +0800

    fix the null error

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
 // let getHumanChoice = () => prompt("Input rock, paper, or scissors: ").toLowerCase().trim();
-function getComputerChoice() {
-  switch (Math.floor(Math.random() * 3)) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
-  }
-}
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-  // your code here!
-  console.log("YOU play " + humanChoice);
-  console.log("AI plays " + computerChoice);
-  if (humanChoice === computerChoice) {
-    console.log("TIE");
-  } else if ((humanChoice === "rock" && computerChoice === "paper")
-    || (humanChoice === "paper" && computerChoice === "scissors")
-    || (humanChoice === "scissors" && computerChoice === "rock")) {
-    computerScore += 1;
-    console.log("AI WINS!");
-  } else if ((computerChoice === "rock" && humanChoice === "paper")
-    || (computerChoice === "paper" && humanChoice === "scissors")
-    || (computerChoice === "scissors" && humanChoice === "rock")) {
-    console.log("YOU WINS!");
-    humanScore += 1;
-  } else {
-    console.log("ERROR! Input a valid choice (rock paper or scissors)!");
-  }
-  console.log("SCORE");
-  console.log("HUMAN: " + humanScore);
-  console.log("AI: " + computerScore);
-}
-
-while (humanScore + computerScore != 5) {
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-}
-
-
+document.addEventListener('DOMContentLoaded', () => {
+  const enterButton = document.querySelector('enterButton');
+  enterButton.addEventListener("click", () => {
+  });
+});
+//
+// function getComputerChoice() {
+//   switch (Math.floor(Math.random() * 3)) {
+//     case 0:
+//       return "rock";
+//     case 1:
+//       return "paper";
+//     case 2:
+//       return "scissors";
+//   }
+// }
+// let humanScore = 0;
+// let computerScore = 0;
+//
+// function playRound(humanChoice, computerChoice) {
+//   // your code here!
+//   console.log("YOU play " + humanChoice);
+//   console.log("AI plays " + computerChoice);
+//   if (humanChoice === computerChoice) {
+//     console.log("TIE");
+//   } else if ((humanChoice === "rock" && computerChoice === "paper")
+//     || (humanChoice === "paper" && computerChoice === "scissors")
+//     || (humanChoice === "scissors" && computerChoice === "rock")) {
+//     computerScore += 1;
+//     console.log("AI WINS!");
+//   } else if ((computerChoice === "rock" && humanChoice === "paper")
+//     || (computerChoice === "paper" && humanChoice === "scissors")
+//     || (computerChoice === "scissors" && humanChoice === "rock")) {
+//     console.log("YOU WINS!");
+//     humanScore += 1;
+//   } else {
+//     console.log("ERROR! Input a valid choice (rock paper or scissors)!");
+//   }
+//   console.log("SCORE");
+//   console.log("HUMAN: " + humanScore);
+//   console.log("AI: " + computerScore);
+// }
+//
+// while (humanScore + computerScore != 5) {
+//   const humanSelection = getHumanChoice();
+//   const computerSelection = getComputerChoice();
+//
+//   playRound(humanSelection, computerSelection);
+//
+// }
+//
+//
 
