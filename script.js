86b38c30d9e9388c70a783ce836419a9a7fced0f
kenaniscoding <banalkenan@gmail.com>
2025-07-21 09:58:06 +0800

    added working score bar

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-// let getHumanChoice = () => prompt("Input rock, paper, or scissors: ").toLowerCase().trim();
-document.addEventListener('DOMContentLoaded', () => {
-  const enterButton = document.querySelector('.enterButton');
-  const humanChoice = document.querySelector('.humanChoice');
-  enterButton.addEventListener("click", () => {
-    // get the humanChoice id
-    alert(humanChoice.value);
-  });
-});
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3)) {
@@ -18,33 +11,49 @@ function getComputerChoice() {
       return "scissors";
   }
 }
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-  // your code here!
-  console.log("YOU play " + humanChoice);
-  console.log("AI plays " + computerChoice);
+  let isHumanWin = 0;
   if (humanChoice === computerChoice) {
     console.log("TIE");
+    isHumanWin = -1;
   } else if ((humanChoice === "rock" && computerChoice === "paper")
     || (humanChoice === "paper" && computerChoice === "scissors")
     || (humanChoice === "scissors" && computerChoice === "rock")) {
     computerScore += 1;
+    isHumanWin = 0;
     console.log("AI WINS!");
   } else if ((computerChoice === "rock" && humanChoice === "paper")
     || (computerChoice === "paper" && humanChoice === "scissors")
     || (computerChoice === "scissors" && humanChoice === "rock")) {
     console.log("YOU WINS!");
+    isHumanWin = 1;
     humanScore += 1;
-  } else {
-    console.log("ERROR! Input a valid choice (rock paper or scissors)!");
   }
-  console.log("SCORE");
-  console.log("HUMAN: " + humanScore);
-  console.log("AI: " + computerScore);
+  return isHumanWin;
 }
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  const enterButton = document.querySelector('.enterButton');
+  const humanChoice = document.querySelector('.humanChoice');
+  const humanScoreTxt = document.getElementById('humanScore');
+  const computerScoreTxt = document.getElementById('computerScore');
+  enterButton.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = humanChoice.value;
+    const isHumanWin = playRound(humanSelection, computerSelection);
+    if (isHumanWin === 1) {
+      alert('HUMAN WINS');
+      humanScoreTxt.innerHTML = 'Human: ' + humanScore;
+    } else if (isHumanWin === 0) {
+      computerScoreTxt.innerHTML = 'Computer: ' + computerScore;
+      alert('AI WINS');
+    } else {
+      alert('TIE');
+    }
+  });
+});
 // while (humanScore + computerScore != 5) {
 //   const humanSelection = getHumanChoice();
 //   const computerSelection = getComputerChoice();
